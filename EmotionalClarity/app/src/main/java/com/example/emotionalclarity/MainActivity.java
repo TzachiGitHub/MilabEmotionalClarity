20c3bcee12357c5d386f39d5d23395685658063f
Tzachi Elrom <Tzachi.Elrom94@gmail.com>
2020-01-21 15:59:26 +0200

    added animation functionality for the teenager.

--- EmotionalClarity/app/src/main/java/com/example/emotionalclarity/MainActivity.java
+++ EmotionalClarity/app/src/main/java/com/example/emotionalclarity/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.emotionalclarity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String TAG = "MainActivity";
+    public Boolean teenagerIsActive = false;
+    ImageView teenagerImageView;
+
+    public void showPicture(View view){
+        Log.i(TAG, "got into cameraOn");
+        ImageView teenagerImageView = findViewById(R.id.teenager);
+        if (!teenagerIsActive) {
+            teenagerIsActive = true;
+            teenagerImageView.animate().alpha(1).setDuration(1000);
+        }
+    }
+
+    public void hidePicture(View view){
+        ImageView teenagerImageView = findViewById(R.id.teenager);
+
+        if(teenagerIsActive){
+            teenagerIsActive = false;
+            teenagerImageView.animate().alpha(0).setDuration(1000);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
 }
