dc3430d94464743c6a6f39991462dacf1ba672d3
Tzachi Elrom <Tzachi.Elrom94@gmail.com>
2020-03-02 10:58:13 +0200

    got the microphone button and the camera button working - they work as activities and not as fragments so more updates regarding these two are required.

---
+++ EmotionalClarity/app/src/main/java/com/example/emotionalclarity/AudioRecordTest.java
@@ -0,0 +1,223 @@
+package com.example.emotionalclarity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import java.io.IOException;
+
+//import android.support.annotation.NonNull;
+//import android.support.v4.app.ActivityCompat;
+//import android.support.v7.app.AppCompatActivity;
+
+public class AudioRecordTest extends AppCompatActivity {
+
+    private static final String TAG_AUDIO_RECORD_TEST = "AudioRecordTest";
+    private static final String TAG_FILENAME = "filename";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String fileName = null;
+
+    private RecordButton recordButton = null;
+    private MediaRecorder recorder = null;
+
+    private PlayButton   playButton = null;
+    private MediaPlayer   player = null;
+
+    private Button finishButton = null;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION){
+            permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(fileName);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(TAG_AUDIO_RECORD_TEST, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        player.release();
+        player = null;
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(fileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(TAG_AUDIO_RECORD_TEST, "prepare() failed");
+        }
+
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+
+    class RecordButton extends androidx.appcompat.widget.AppCompatButton {
+        boolean mStartRecording = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    setText("Stop recording");
+                } else {
+                    setText("Start recording");
+                }
+                mStartRecording = !mStartRecording;
+            }
+        };
+
+        public RecordButton(Context ctx) {
+            super(ctx);
+            setText("Start recording");
+            setOnClickListener(clicker);
+        }
+    }
+
+    class PlayButton extends androidx.appcompat.widget.AppCompatButton {
+        boolean mStartPlaying = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    setText("Stop playing");
+                } else {
+                    setText("Start playing");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        };
+
+        public PlayButton(Context ctx) {
+            super(ctx);
+            setText("Start playing");
+            setOnClickListener(clicker);
+        }
+    }
+
+    class FinishButton extends androidx.appcompat.widget.AppCompatButton {
+        boolean mStartPlaying = true;
+
+        OnClickListener finishClicker = new OnClickListener() {
+            public void onClick(View v) {
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra(TAG_FILENAME, fileName);
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        };
+
+        public FinishButton(Context context) {
+            super(context);
+            setText("Finish");
+            setOnClickListener(finishClicker);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        try {
+            // Record to the external cache directory for visibility
+            fileName = getExternalCacheDir().getAbsolutePath();
+            fileName += "/audiorecordtest.3gp";
+
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+            LinearLayout ll = new LinearLayout(this);
+            recordButton = new RecordButton(this);
+            ll.addView(recordButton,
+                    new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            0));
+            playButton = new PlayButton(this);
+            ll.addView(playButton,
+                    new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            0));
+
+            finishButton = new FinishButton(this);
+
+            ll.addView(finishButton,
+                    new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            0));
+
+            setContentView(ll);
+
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            Log.e(TAG_AUDIO_RECORD_TEST, "Function getAbsolutePath malfunctioned - made a NullPointException");
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (recorder != null) {
+            recorder.release();
+            recorder = null;
+        }
+
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+}
--- EmotionalClarity/app/src/main/java/com/example/emotionalclarity/MainActivity.java
+++ EmotionalClarity/app/src/main/java/com/example/emotionalclarity/MainActivity.java
@@ -1,45 +1,112 @@
 package com.example.emotionalclarity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    public String TAG = "MainActivity";
-    public Boolean teenagerIsActive = false;
-    ImageView teenagerImageView;
-
-    public void showPicture(View view){
-        Log.i(TAG, "got into cameraOn");
-        ImageView teenagerImageView = findViewById(R.id.teenager);
-        if (!teenagerIsActive) {
-            teenagerIsActive = true;
-            teenagerImageView.animate().alpha(1).setDuration(1000);
-        }
-    }
-
-    public void hidePicture(View view){
-        ImageView teenagerImageView = findViewById(R.id.teenager);
-
-        if(teenagerIsActive){
-            teenagerIsActive = false;
-            teenagerImageView.animate().alpha(0).setDuration(1000);
-        }
-    }
+    private static final String TAG_MAINACTIVITY = "MainActivity";
+    private static final int REQUEST_IMAGE_CAPTURE = 202;
+    private static final int REQUEST_CAMERA_USE = 201;
+    private static final int MIC_REQUEST_CODE = 200;
+    private static final String TAG_FILENAME = "filename";
+    private static String micFileName = null;
+    ImageButton micStart, cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        micStart = (ImageButton) findViewById(R.id.micStart);
+        micStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent micIntent = new Intent(MainActivity.this, AudioRecordTest.class);
+                MainActivity.this.startActivityForResult(micIntent, MIC_REQUEST_CODE);
+            }
+        });
 
+        //handler for the camera button - for personal pictures
+        cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //defining the required permissions for camera use
+                String[] premissionsToAsk = {Manifest.permission.CAMERA};
+                // asking for camera permission
+                ActivityCompat.requestPermissions(MainActivity.this, premissionsToAsk, REQUEST_CAMERA_USE);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                } else {
+                    Log.e(TAG_MAINACTIVITY, "cameraButton onClick has Failed");
+                    Toast.makeText(MainActivity.this, "cameraButton onClick has Failed!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+            try{
+                //handler for the Audio-Input Activity (Microphone)
+                if(requestCode == MIC_REQUEST_CODE && resultCode == RESULT_OK){
+                    // get the microphone input from the activity and display on screen
+                    if(data != null && data.hasExtra(TAG_FILENAME)){
+                        micFileName = data.getExtras().getString(TAG_FILENAME);
+                        ImageButton playButton = (ImageButton) findViewById(R.id.playButton);
+                        playButton.setVisibility(View.VISIBLE);
+                    }
+
+                    //handler for the Camera-Activity
+                } else if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+                    Toast.makeText(this, "onActivityResult was activated - into the else - as in camera activity was initiated", Toast.LENGTH_LONG).show();
+                    Log.i(TAG_MAINACTIVITY, "Camera Activity returned!");
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    ImageView picture = (ImageView) findViewById(R.id.pictureImageView);
+                    Log.i(TAG_MAINACTIVITY,"I got to the picture setting part!!! trying to put it now!");
+                    picture.setImageBitmap(imageBitmap);
+                }
+                //catches the NullPointerException made by hasExtra method or getString method
+            }catch (NullPointerException e){
+                e.printStackTrace();
+                Log.e(TAG_MAINACTIVITY, "Method hasExtra or getString failed - onActivityResult");
+            }
+    }
+
+
+    public void micOutputListener(View v){
+        if(micFileName == null) return;
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(micFileName);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch(IOException e){
+            Log.e("MainActivity", "Mic activity result failed");
+            e.printStackTrace();
+        }
 
     }
 
